802edcce4831ae710ce1bf5b2bb5ba8f8948af44
cooker-ru <38529351+cooker-ru@users.noreply.github.com>
2019-04-27 10:59:35 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -215,43 +215,4 @@ document.addEventListener('DOMContentLoaded', function() {
    });
  });
 
-// ------------ svg animation ----------- \\
-
-// function animationLazy(){
-//
-//   document.onreadystatechange = () => {
-//
-//     if (document.readyState === 'complete') {
-//
-//       // let el = document.querySelector('#meat');
-//       // let animationMeat = new LazyLinePainter(el, {
-//       //   "ease":"easeLinear",
-//       //   "strokeWidth": 5,
-//       //   "strokeOpacity": 1,
-//       //   "strokeColor": "#ffffff",
-//       //   "strokeCap": "square",
-//       //   "delay" : 5000,
-//       //   "reverse" : true,
-//       //   "repeat": 0
-//       // });
-//       // animationMeat.paint();
-//
-//       let elCat = document.querySelector('#cat');
-//       let animationCat = new LazyLinePainter(elCat, {
-//         "strokeWidth": 3,
-//         "strokeOpacity": 1,
-//         "strokeColor": "#26A69A",
-//         "strokeCap": "square",
-//         "delay" : 7000,
-//         "repeat": 0
-//       });
-//       animationCat.paint();
-//     }
-//   }
-//
-// };
-// document.addEventListener('DOMContentLoaded', animationLazy);
-//
-
-
 // ----------- index.js ------------- \\
